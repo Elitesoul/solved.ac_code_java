7387dbce39f89c023a54e84940d87657c3b55d6d
Elitesoul <elitesoul@daum.net>
2023-05-02 21:13:25 +0900

    Create Template.java

---
+++ Template.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Template {
+    
+    static int n;
+    static int answer;
+    static int[][] mat;
+    
+    static String[] array;
+    static BufferedReader br;
+    static StringBuilder sb;
+    
+    public static void main(String[] args) throws IOException {
+        
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        
+        System.out.println(sb);
+        br.close();
+    }
+}
