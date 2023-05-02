ff1ccac66b5a272be05d452e2f8bd9d99a3b6571
Elitesoul <55022875+Elitesoul@users.noreply.github.com>
2023-05-02 16:30:05 +0900

    Create BOJ_7287.java

---
+++ Sprout/Input/BOJ_7287.java
@@ -0,0 +1,9 @@
+//BOJ 7287 등록
+//https://www.acmicpc.net/problem/7287
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(4);
+        System.out.println("elitesoul");
+    }
+}
